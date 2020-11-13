06ca1ce655843920e2af7470496cb94140fba56b
afeiship <1290657123@qq.com>
2020-11-13 16:53:51 +0800

    feat: add customize unit test

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,12 +1,15 @@
-(function() {
-  var nx = require('@jswork/next');
-  var NxDtLog = require('../src/index');
+(function () {
+  const NxDtLog = require('../src/index');
 
-  describe('NxDtLog.methods', function() {
-    test('date/time/datetime', function() {
+  describe('NxDtLog.methods', function () {
+    test('use std api: date/time/datetime', function () {
       NxDtLog.time('i will print current time');
       NxDtLog.date('i will print', 'current', 'date');
-      NxDtLog.datetime('i will','print current datetime');
+      NxDtLog.datetime('i will', 'print current datetime');
+    });
+
+    test('use customize api: log', () => {
+      NxDtLog.log(['a', 'b', 'c', 'yy-mm-dd']);
     });
   });
 })();
